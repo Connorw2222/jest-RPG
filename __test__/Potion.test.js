a095660e3145b06b21331fe9643ebbe807909189
Connorw2222 <Connor2Werth@gmail.com>
2021-08-18 13:41:18 -0700

    added in the write stats and get inventory methods

--- __test__/Potion.test.js
+++ __test__/Potion.test.js
@@ -11,13 +11,6 @@ test('creates a random potion object', () => {
 const potion = new Potion();
 
     expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
+expect(potion.name.length).toBeGreaterThan(0);
+expect(potion.value).toEqual(expect.any(Number));
 });
-test('creates a random potion object', () => {
-    const potion = new Potion();
-  
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
-  });
